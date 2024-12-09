cb4482ef259757606475229874b088dab12a9b4a
mordak <bmickael@proton.me>
2024-12-09 20:25:02 +0100

    fix build zoom on osx

--- extract.so.sh
+++ extract.so.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 tar -xJf archive.tar.xz
-ln -s libmeetingsdk.so zoom-meeting-sdk-linux/libmeetingsdk.so.1 -f
+ln -sf libmeetingsdk.so zoom-meeting-sdk-linux/libmeetingsdk.so.1
 exit $?
